ba695f56041cd1c3d6f3a452b76cf95a0d4762c2
Mahesh <88389547+Mahesh09494@users.noreply.github.com>
2021-12-22 18:28:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ variable "env" {
 }
 
 
-resource "aws_s3_bucket" "Maheshs3-123" {
+resource "aws_s3_bucket" "Maheshs3" {
     count = "${var.env == "true"? 1: 0}"
-    bucket = "Maheshs3-demo"
+    bucket = "Maheshs3demo"
 }
 
-resource "aws_instance" "Maheshaws-poc"{
+resource "aws_instance" "Maheshawspoc"{
     count = "${var.env == "false" ? 1: 0}"
     ami = "ami-0b9064170e32bde34"
     instance_type = "t2.micro" 
